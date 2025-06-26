42af6b6d46ab869cf0ea073e9ec892aac7b6fcbc
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-26 19:12:05 -0400

    Final Project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String party = ""; // String party = ""
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your party affiliation. [DRI]: "); // output “Enter your party affiliation. D for Democrat, R for Republican, or I for Independent: “
+        party = in.nextLine();  // input party
+        party = party.toUpperCase(); // change to uppercase
+
+        switch (party)
+        {
+            case "D": // if party == "D" then
+                System.out.println("You get a Democratic Donkey."); // output "You get a Democratic Donkey."
+                break;
+            case "R": // else if party == "R" then
+                System.out.println("You get a Republican Elephant."); // output "You get a Republican Elephant."
+                break;
+            case "I": // else if party == "I" then
+                System.out.println("You get an Independent Man."); // output "You get an Independent Man."
+                break;
+            default:
+                System.out.println("You are affiliated with another party, named: " + party + "."); // output "You are affiliated with another party, named" + party + "."
+                break;
+        } // end if
     }
 }
